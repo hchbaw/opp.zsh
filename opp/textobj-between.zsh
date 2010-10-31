2bacdd8249aefe8d8322d9acb4db57d96198534c
Takeshi Banse <takebi@laafc.net>
2010-11-01 00:42:00 +0900

    Added textobj-between.
    Signed-off-by: Takeshi Banse <takebi@laafc.net>

---
+++ opp/textobj-between.zsh
@@ -0,0 +1,20 @@
+#!zsh
+# textobj-between code.
+# Thank you very much, thinca and tarao!
+#
+# http://d.hatena.ne.jp/thinca/20100614/1276448745
+# http://d.hatena.ne.jp/tarao/20100715/1279185753
+
+def-oppc-textobj-between () {
+  def-oppc-inbetween-2 "$1" "opp+i$1" "opp+a$1" oppc-tb-main
+}
+
+oppc-tb-main () {
+  shift
+  local c; read -s -k 1 c
+  [[ "$c" == [[:print:]] ]] || return
+  zle -U "$c"
+  "$@"
+}
+
+def-oppc-textobj-between 'F'
