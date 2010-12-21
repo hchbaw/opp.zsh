6e35943bb2865221c2d0f69641e01a486e194bd1
Takeshi Banse <takebi@laafc.net>
2010-12-22 00:00:01 +0900

    Spelling corrections.
    Signed-off-by: Takeshi Banse <takebi@laafc.net>

--- opp.zsh
+++ opp.zsh
@@ -1,7 +1,7 @@
 # vim's text-objects-ish for zsh.
 
 # Author: Takeshi Banse <takebi@laafc.net>
-# Licence: Public Domain
+# License: Public Domain
 
 # Thank you very much, Bram Moolenaar!
 # I want to use the vim's text-objects in zsh.
@@ -9,11 +9,11 @@
 # To use this,
 # 1) source this file.
 # % source opp.zsh
-# 2) If you wish to load the extentions, source them afterward.
+# 2) If you wish to load the extensions, source them afterward.
 # % source opp/*.zsh
 #
 # *Optionally* you can use the zcompiled file with the autoloading for a
-# little faster loading on every shell startup, if you zcompile the
+# little faster loading on every shell start up, if you zcompile the
 # necessary functions.
 # *1) Prepare zcompiling the defined functions and the install command.
 # % OS=(~/path/to/opp.zsh/{opp.zsh,opp/*.zsh})
@@ -23,13 +23,12 @@
 # % OS=(~/path/to/opp.zsh/{opp.zsh,opp/*.zsh} ~/.zsh/opp-surround.zsh(N))
 # *3) Generate the ~/.zsh/zfunc/{opp,opp-install}.zwc.
 # % (zsh -c "for O in $OS;do . \$O;done && opp-zcompile $OS[1] ~/.zsh/zfunc"
-# *4) Source the zcompiled install command file insted of this file, and
-# set up an autaload clause apprpriately.
+# set up an autaload clause appropriately.
 # % { . ~/.zsh/zfunc/opp-install; opp-install }
 # % autoload opp
 
 # Note:
-# This script replaces below vicmd kepmap entries.
+# This script replaces below vicmd key map entries.
 #   ~, c, d, gu, gU and y
 # Please beware of.
 
