873762447b3487fb030df6273e0be65757a87c2f
Takeshi Banse <takebi@laafc.net>
2013-04-06 08:40:48 +0900

    Propagate zle result
    but the caller side functions (opp-generic) do not do anything…
    
    Signed-off-by: Takeshi Banse <takebi@laafc.net>

--- opp.zsh
+++ opp.zsh
@@ -314,12 +314,15 @@ oppc-inbetween-main () {
 
 opp-vi-prev-repeat-find () {
   zle vi-rev-repeat-find
+  local ret=$?
+  ((ret==0)) || return ret
   # http://zsh.git.sourceforge.net/git/gitweb.cgi?p=zsh/zsh;a=commitdiff;h=3e39278c24ff7b5cd686af9b219178e94d9228ad
   autoload -Uz is-at-least
   if is-at-least 5.0.1 || { [[ -n "${ZSH_PATCHLEVEL-}" ]] && \
      is-at-least 1.5687 "${ZSH_PATCHLEVEL}" }; then
     ((CURSOR+=2))
   fi
+  return ret
 }
 zle -N opp-vi-prev-repeat-find
 
